51c16759cc49260afb6acede2887c17eda63308e
peterchencc <peterchencc@gmail.com>
2021-04-05 17:49:08 +0200

    remove default navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
